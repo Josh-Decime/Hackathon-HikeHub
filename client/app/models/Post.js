283cb8e9815e82c07178112346cfcc303d701f19
Josh <josh_decime@yahoo.com>
2023-12-22 17:41:13 -0700

    Base styling is looking gooood

--- client/app/models/Post.js
+++ client/app/models/Post.js
@@ -6,18 +6,27 @@ export class Post {
         this.imgUrl = data.imgUrl
         this.authorId = data.authorId
         this.author = data.author
+        this.popularity = data.popularity
         this.createdAt = new Date(data.createdAt)
         this.updatedAt = new Date(data.updatedAt)
     }
 
     get PostTemplate() {
         return `
-         <div class="foreground-orange rounded mt-3">
+        <div class="foreground-orange rounded mt-3">
             <p class="fs-3 px-3 my-0">${this.title}</p>
             <p class="px-3">${this.description}</p>
-            <img class="img-container"
-                src="${this.imgUrl}"
-                alt="Picture of ${this.title}">
+            <img class="img-container" src="${this.imgUrl}" alt="Picture of ${this.title}">
+            <div class="d-flex justify-content-between">
+                <button type="button" class="btn wood-brown light-text m-2" data-bs-toggle="modal"
+                    data-bs-target="#commentModal" id="${this.id}">
+                    Comments
+                </button>
+                <div class="">
+                    <span class="btn"><i class="mdi mdi-arrow-up-bold-box fs-1"></i></span>
+                    <span class="btn"><i class="mdi mdi-arrow-down-bold-box fs-1"></i></span>
+                </div>
+            </div>
         </div>
         `
     }
